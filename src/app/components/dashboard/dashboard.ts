bbaa308f8b27ad4d8279bdc7208ecbf602e774cc
wallyssonmar <wallysson.marcos@outlook.com>
2025-08-15 12:13:05 -0300

    Feito o carrossel das imagens dos projetos

--- src/app/components/dashboard/dashboard.ts
+++ src/app/components/dashboard/dashboard.ts
@@ -1,33 +1,35 @@
-import { Component, model } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { GalleriaModule } from 'primeng/galleria';
 import { ButtonModule } from 'primeng/button';
-import { Image, ImageModule } from 'primeng/image';
-
+import {  ImageModule } from 'primeng/image';
 
 import { RouterLink, RouterOutlet } from '@angular/router';
 
 import { CommonModule } from '@angular/common';
 
-
-
-
 @Component({
   selector: 'app-dashboard',
-  imports: [CommonModule, GalleriaModule, ButtonModule,ImageModule,Image],
-  
+  imports: [CommonModule, GalleriaModule, ButtonModule, ImageModule,GalleriaModule],
+
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
 export class Dashboard {
   isHovered = false;
   activeSection: string = 'sobre';
+  images: any[] = [];
+  
+  ngOnInit(){
+    this.images = [
+      {projetoPriorize : '/sherek.jpg'},
+      {projetoPriorize : '/git.png'},
+      {projetoPriorize : '/sherek.jpg'},
+    ]
+  }
 
   scrollTo(sectionId: string) {
     document.getElementById(sectionId)?.scrollIntoView({ behavior: 'smooth' });
   }
-
- 
-
 
   onMouseEnter() {
     this.isHovered = true;
@@ -36,6 +38,18 @@ export class Dashboard {
   onMouseLeave() {
     this.isHovered = false;
   }
-
-  
+  responsiveOptions: any[] = [
+    {
+      breakpoint: '991px',
+      numVisible: 4,
+    },
+    {
+      breakpoint: '767px',
+      numVisible: 3,
+    },
+    {
+      breakpoint: '575px',
+      numVisible: 1,
+    },
+  ];
 }
--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,19 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 import { provideAnimations } from '@angular/platform-browser/animations';
-
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
 
 bootstrapApplication(App, {
   ...appConfig,
   providers: [
     ...(appConfig.providers || []),
-    provideAnimations()
+    provideAnimations(),
+    providePrimeNG({
+            theme: {
+                preset: Aura
+            }
+          })
   ]
 }).catch((err) => console.error(err));
 
