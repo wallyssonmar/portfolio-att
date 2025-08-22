8048c6b9056b97970391444f072f61b3acfdf36a
wallyssonmar <wallysson.marcos@outlook.com>
2025-08-21 21:10:46 -0300

    terminei alguns ajustes que faltava e fiz deixei responsivo

--- src/app/components/dashboard/dashboard.ts
+++ src/app/components/dashboard/dashboard.ts
@@ -2,6 +2,7 @@ import { Component, OnInit} from '@angular/core';
 import { GalleriaModule } from 'primeng/galleria';
 import { ButtonModule } from 'primeng/button';
 import {  ImageModule } from 'primeng/image';
+import { MenuModule } from 'primeng/menu';
 
 import { RouterLink, RouterOutlet } from '@angular/router';
 
@@ -9,7 +10,7 @@ import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [CommonModule, GalleriaModule, ButtonModule, ImageModule,GalleriaModule],
+  imports: [CommonModule, GalleriaModule, ButtonModule, ImageModule,MenuModule],
 
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
@@ -18,6 +19,7 @@ export class Dashboard {
   isHovered = false;
   activeSection: string = 'sobre';
   images: any[] = [];
+  items : any[] = [];
   
   ngOnInit(){
     this.images = [
@@ -25,6 +27,31 @@ export class Dashboard {
       {projetoPriorize : '/priorize/priorize-2.png'},
       {projetoPriorize : '/priorize/priorize-3.png'},
     ]
+
+    this.items = [
+            {
+                
+    label: 'Sobre',
+    icon: 'pi pi-user',
+    command: () => this.scrollTo('sobre')
+  },
+  {
+    label: 'Habilidades',
+    icon: 'pi pi-star',
+    command: () => this.scrollTo('habilidades')
+  },
+  {
+    label: 'Projetos',
+    icon: 'pi pi-briefcase',
+    command: () => this.scrollTo('projetos')
+  },
+  {
+    label: 'Contato',
+    icon: 'pi pi-envelope',
+    command: () => this.scrollTo('contato')
+  }
+            
+        ];
   }
 
   scrollTo(sectionId: string) {
